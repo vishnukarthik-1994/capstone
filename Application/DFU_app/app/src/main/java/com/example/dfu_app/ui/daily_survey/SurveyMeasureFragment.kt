2528bab51c5a65be632c2adee281f8440f842aa6
Yi Jin <yijinsx@uw.edu>
2022-05-21 13:51:38 -0700

    bluetooth connection code

--- Application/DFU_app/app/src/main/java/com/example/dfu_app/ui/daily_survey/SurveyMeasureFragment.kt
+++ Application/DFU_app/app/src/main/java/com/example/dfu_app/ui/daily_survey/SurveyMeasureFragment.kt
@@ -1,42 +1,93 @@
 package com.example.dfu_app.ui.daily_survey
 
+import android.Manifest
+import android.R
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.ContentValues
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.findNavController
 import com.example.dfu_app.databinding.FragmentSurveyMeasurementBinding
 import com.example.dfu_app.ui.error_message.ErrorMessage.setErrorMessage
 import java.io.IOException
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
+import com.punchthrough.blestarterappandroid.ble.ConnectionEventListener
+import com.punchthrough.blestarterappandroid.ble.ConnectionManager
+import kotlinx.android.synthetic.main.activity_main.scan_button
+import kotlinx.android.synthetic.main.activity_main.scan_results_recycler_view
+import org.jetbrains.anko.alert
+import timber.log.Timber
 
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
 
 class SurveyMeasureFragment: Fragment() {
 
     private val shareViewModel: SurveyViewModel by activityViewModels()
     private var _binding: FragmentSurveyMeasurementBinding? = null
+    private val scanResults = mutableListOf<ScanResult>()
+    private var isScanning = false
+    private val scanResultAdapter: ScanResultAdapter by lazy {
+        ScanResultAdapter(scanResults) {
+                result ->
+            if (isScanning) {
+                stopBleScan()
+            }
+            with(result.device) {
+                Timber.w("Connecting to $address")
+                ConnectionManager.connect(this, this@MainActivity)
+            }
+        }
+    }
     // This property is only valid between onCreateView and
     // onDestroyView.
     private lateinit var bluetoothManager: BluetoothManager
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     private val binding get() = _binding!!
-    private val REQUEST_ENABLE_BLUETOOTH = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bluetoothManager = (requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager)
@@ -80,9 +131,141 @@ class SurveyMeasureFragment: Fragment() {
             /*
             When measure start should receive data from arduino
             * */
-            TODO()
+            startBleScan()
         }
     }
+    private fun startBleScan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted!!) {
+            requestLocationPermission()
+        }
+        else {
+            scanResults.clear()
+            scanResultAdapter.notifyDataSetChanged()
+            bleScanner.startScan(null, scanSettings, scanCallback)
+            isScanning = true
+
+        }
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val indexQuery = scanResults.indexOfFirst { it.device.address == result.device.address }
+            if (indexQuery != -1) { // A scan result already exists with the same address
+                scanResults[indexQuery] = result
+                scanResultAdapter.notifyItemChanged(indexQuery)
+            } else {
+                with(result.device) {
+                   //  Log.i(“ScanCallback”, "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
+                }
+                scanResults.add(result)
+                scanResultAdapter.notifyItemInserted(scanResults.size - 1)
+            }
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            // Log.e(“ScanCallback”, "onScanFailed: code $errorCode")
+        }
+    }
+
+    private val scanResultAdapter: ScanResultAdapter by lazy {
+        ScanResultAdapter(scanResults) { result ->
+            // User tapped on a scan result
+            if (isScanning) {
+                stopBleScan()
+            }
+            with(result.device) {
+                Log.w("ScanResultAdapter", "Connecting to $address")
+                connectGatt(context, false, gattCallback)
+            }
+        }
+    }
+
+    private val gattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            val deviceAddress = gatt.device.address
+
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.w("BluetoothGattCallback", "Successfully connected to $deviceAddress")
+                    // TODO: Store a reference to BluetoothGatt
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.w("BluetoothGattCallback", "Successfully disconnected from $deviceAddress")
+                    gatt.close()
+                }
+            } else {
+                Log.w("BluetoothGattCallback", "Error $status encountered for $deviceAddress! Disconnecting...")
+                gatt.close()
+            }
+        }
+    }
+
+
+    // ble scan
+    val filter = ScanFilter.Builder().setServiceUuid(
+        ParcelUuid.fromString(ENVIRONMENTAL_SERVICE_UUID.toString())
+    ).build()
+
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+        isScanning = false
+    }
+
+
+
+
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            with(result.device) {
+                // Log.i(“ScanCallback”, "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
+            }
+        }
+    }
+
+    // ask for fine location permission
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted == true) {
+            return
+        }
+        // val alertDialog = AlertDialog.Builder(activity, R.style.Theme_AppCompat_Dialog_Alert).create()
+        activity?.runOnUiThread {
+            val dialog: AlertDialog = AlertDialog.Builder(activity)
+                .setCancelable(false)
+                .setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
+                        "location access in order to scan for BLE devices.")
+                .setTitle("Location permission required").show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+                    requestLocationPermission()
+                } else {
+                    startBleScan()
+                }
+            }
+        }
+    }
+
+    // ask for permission location
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    }
+
     //ask bluetooth permission
     private fun askBluetoothPermission() {
         if (!bluetoothAdapter.isEnabled) {
@@ -90,6 +273,7 @@ class SurveyMeasureFragment: Fragment() {
             resultLauncher.launch(enableBtIntent)
         }
     }
+
     // sample code to get data from arduino
     private fun readBlueToothDataFromMotherShip(bluetoothSocket: BluetoothSocket) {
         Log.i(ContentValues.TAG, Thread.currentThread().name)
@@ -107,6 +291,15 @@ class SurveyMeasureFragment: Fragment() {
             }
         }
     }
+
+
+    private val isLocationPermissionGranted
+        get() = activity?.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
     //Put away keyboard if unnecessary
     private fun closeKeyBoards(){
         val manager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -116,3 +309,5 @@ class SurveyMeasureFragment: Fragment() {
         )
     }
 }
+
+
